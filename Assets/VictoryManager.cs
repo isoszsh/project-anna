e5352ed36f496ebf64f0296f1c4f5d0d99fb1fd7
isoszsh <isoszsh34@gmail.com>
2024-07-31 10:55:39 +0300

    Level3 Victory

---
+++ Assets/VictoryManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryManager : MonoBehaviour
+{
+
+    public AudioSource aus;
+    public AudioClip horn;
+
+    public GameObject darkenPanel;
+
+
+    void Start()
+    {
+        StartCoroutine(Victory());
+    }
+
+
+    IEnumerator Victory()
+    {
+        aus.PlayOneShot(horn);
+        yield return new WaitForSeconds(0.5f);
+        GameManager.Instance.playerController.playerAnimator.SetTrigger("Wave");
+        yield return new WaitForSeconds(1);
+        darkenPanel.SetActive(true);
+        darkenPanel.GetComponent<Animator>().SetTrigger("Darken");
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadSceneAsync("Level4");
+
+    }
+
+    
+}
