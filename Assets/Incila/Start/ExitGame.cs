da229510f0f33c9f61bd4a9de0038b0dcb2453ff
IbrmSerhat <97800760+IbrmSerhat@users.noreply.github.com>
2024-08-02 22:20:37 +0300

    sandık bossu nerf

--- Assets/Incila/Start/ExitGame.cs
+++ Assets/Incila/Start/ExitGame.cs
@@ -6,13 +6,12 @@ public class ExitGame : MonoBehaviour
 {
     public void QuitGame()
     {
-        
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        
-#endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
 
-        Debug.Log("Oyun kapatýldý!");
+                Debug.Log("Oyun kapatÄ±ldÄ±!");
     }
 }
